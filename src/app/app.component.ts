92beba39c6726edfda3e8655e8d61aab54abd83d
danielrsant <danrsant14@gmail.com>
2020-04-14 20:27:22 -0300

    creanting new rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'Game';
 
   totalCards = [0, 0, 0, 0, 0];
-  cardNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
+  cardNumbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', '1'];
   cardTypes = ['P', 'O', 'C', 'E'];
 
   allCards = [];
@@ -28,10 +28,10 @@ export class AppComponent implements OnInit, OnDestroy {
   winnersPlayer2 = 0;
   cardsPlayedPlayer2 = [];
 
-
   destroy$ = new Subject<boolean>();
 
   firstPlayer = null;
+  canPlay = null;
 
   constructor(private _formBuilder: FormBuilder, private toastr: ToastrService) { }
 
@@ -65,26 +65,114 @@ export class AppComponent implements OnInit, OnDestroy {
   setLength() {
     this.player1 = this.formPlayer1.get('options') as FormArray;
     this.player2 = this.formPlayer2.get('options') as FormArray;
-    
+
     this.totalCards.map(() => {
       this.player1.push(this.createControls());
       this.player2.push(this.createControls());
     });
   }
 
-  setMovePlayer1(item) {
+  setMovePlayer1(control) {
+    if (!this.formPlayer2.get('selected').value && this.canPlay) {
+      if (this.canPlay !== 'player1') {
+        this.toastr.error(`Jogador 2 começa!!`);
+        return;
+      }
+    }
+
+    const card = control.get('card').value;
+
     if (!this.formPlayer1.get('selected').value) {
-      this.cardsPlayedPlayer1.push(item.get('card').value);
-      this.formPlayer1.get('selected').setValue(item.get('card').value);
-      item.get('card').setValue('');
+
+      const isValid = this.verifyPlay1(card);
+
+      if (isValid) {
+        this.cardsPlayedPlayer1.push(card);
+        this.formPlayer1.get('selected').setValue(card);
+        control.get('card').setValue('');
+      }
     }
   }
 
-  setMovePlayer2(item) {
+  setMovePlayer2(control) {
+    if (!this.formPlayer1.get('selected').value && this.canPlay) {
+      if (this.canPlay !== 'player2') {
+        this.toastr.error(`Jogador 1 começa!!`);
+        return;
+      }
+    }
+
+    const card = control.get('card').value;
+
     if (!this.formPlayer2.get('selected').value) {
-      this.cardsPlayedPlayer2.push(item.get('card').value);
-      this.formPlayer2.get('selected').setValue(item.get('card').value);
-      item.get('card').setValue('');
+
+      const isValid = this.verifyPlay2(card);
+
+      if (isValid) {
+        this.cardsPlayedPlayer2.push(card);
+        this.formPlayer2.get('selected').setValue(card);
+        control.get('card').setValue('');
+      }
+    }
+  }
+
+  verifyPlay1(played) {
+    let isValid = true;
+
+    if (!this.formPlayer2.get('selected').value) {
+      return isValid;
+    }
+
+    const typePlayer2 = this.formPlayer2.get('selected').value.substring(this.formPlayer2.get('selected').value.length - 1);
+    let typePlayer1 = played.substring(played.length - 1);
+
+    if (typePlayer2 === typePlayer1) {
+      return isValid;
+    } else {
+      this.formPlayer2.get('options').value.map(element => {
+        typePlayer1 = element.card.substring(element.card.length - 1);
+        if (typePlayer1 === typePlayer2) {
+          isValid = false;
+        }
+        return element;
+      });
+    }
+
+    if (!isValid) {
+      this.toastr.error(`Carta Inválida!!`);
+      return isValid;
+    } else {
+      return isValid;
+    }
+  }
+
+  verifyPlay2(played) {
+    let isValid = true;
+
+    if (!this.formPlayer1.get('selected').value) {
+      return isValid;
+    }
+
+    const typePlayer1 = this.formPlayer1.get('selected').value.substring(this.formPlayer1.get('selected').value.length - 1);
+    let typePlayer2 = played.substring(played.length - 1);
+
+    if (typePlayer1 === typePlayer2) {
+      return isValid;
+    } else {
+      this.formPlayer2.get('options').value.map(element => {
+        typePlayer2 = element.card.substring(element.card.length - 1);
+        if (typePlayer2 === typePlayer1) {
+          isValid = false;
+        }
+        return element;
+      });
+    }
+
+    if (!isValid) {
+      this.toastr.error(`Carta Inválida!!`);
+      return isValid;
+    } else {
+      return isValid;
     }
   }
 
@@ -102,9 +190,11 @@ export class AppComponent implements OnInit, OnDestroy {
         this.toastr.success(`${this.firstPlayer} venceu!!`);
       } else {
         if (indexCardPlayer1 > indexCardPlayer2) {
+          this.canPlay = 'player1'
           this.toastr.success('Jogador 1 venceu!!');
           this.winnersPlayer1++;
         } else {
+          this.canPlay = 'player2'
           this.toastr.success('Jogador 2 venceu!!');
           this.winnersPlayer2++;
         }
@@ -123,12 +213,13 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   clearPlays() {
-      this.formPlayer1.get('selected').setValue(null);
-      this.formPlayer2.get('selected').setValue(null);
+    this.firstPlayer = null;
+    this.formPlayer1.get('selected').setValue(null);
+    this.formPlayer2.get('selected').setValue(null);
   }
 
   reloadGame() {
-    window.location.reload(); 
+    window.location.reload();
   }
 
   ngOnDestroy(): void {
